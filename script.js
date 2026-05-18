6c4aeff6b051ab7e08285f329882bf29a0dcbf86
QamariyaAlbadi <qamariyaalbadi2005@gmail.com>
2026-05-18 22:35:02 +0400

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ cityInput.addEventListener("keydown", (event) => {
 async function getWeather() {
     const city = cityInput.value.trim();
 
-    const apiKey = "b74879d03adda29a17732b0f673d590c";
+    const apiKey = "API-KEY";
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     
@@ -65,7 +65,7 @@ function getLocationWeather() {
     navigator.geolocation.getCurrentPosition(async (position) => { 
         const lat = position.coords.latitude; 
         const lon = position.coords.longitude; 
-        const apiKey = "b74879d03adda29a17732b0f673d590c"; 
+        const apiKey = "API-KEY"; 
         const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`; 
         const response = await fetch(url); 
         const data = await response.json(); 
